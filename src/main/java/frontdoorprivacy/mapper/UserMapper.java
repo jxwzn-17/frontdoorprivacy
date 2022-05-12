d08414d9495cf77e27c7a0b4ce67819d493f0ccf
jxwzn-17 <g1990107@naver.com>
2022-05-12 23:33:46 +0900

    데베 연동

---
+++ src/main/java/frontdoorprivacy/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package frontdoorprivacy.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+
+public interface UserMapper {
+
+
+}
