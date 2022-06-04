ccaa54a9278bdc40b8a43db970cc718d8e747899
shinyehwan <jghisjl007@naver.com>
2022-06-05 04:57:11 +0900

    shin

---
+++ src/main/java/frontdoorprivacy/model/user/BasketReq.java
@@ -0,0 +1,25 @@
+package frontdoorprivacy.model.user;
+
+
+public class BasketReq {
+    private int p_USID;
+    private  int p_PDID;
+
+
+    public int getP_PDID() {
+        return p_PDID;
+    }
+
+    public void setP_PDID(int p_PDID) {
+        this.p_PDID = p_PDID;
+    }
+
+
+    public int getP_USID() {
+        return p_USID;
+    }
+
+    public void setP_USID(int p_USID) {
+        this.p_USID = p_USID;
+    }
+}
