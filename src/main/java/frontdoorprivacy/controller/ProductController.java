645893818094bd401267f92232bdbc27ab9448ad
jxwzn-17 <g1990107@naver.com>
2022-06-08 17:10:37 +0900

    카테고리 페이지 demo

--- src/main/java/frontdoorprivacy/controller/ProductController.java
+++ src/main/java/frontdoorprivacy/controller/ProductController.java
@@ -130,7 +130,21 @@ public class ProductController {
         return new ResponseEntity<>(mypageProduct , HttpStatus.OK);
 
     }
-
+//    @PostMapping("/mypage/company/edid")
+//    public ResponseEntity<?> upadateMyProduct(@RequestPart(value = "multipartFile", required = false) MultipartFile multipartFile,
+//                                           @RequestPart(value = "productReq") UpdateMypageProduct updateMypageProduct,
+//                                           @RequestPart(value = "detailFile", required = false) MultipartFile detailFile
+//
+//    ) throws IOException {
+//        int pdid = updateMypageProduct.getP_PDID();
+//        FileInfo fileInfo = productService.getfileInfo(pdid);
+//        if(!fileInfo.getImageFileName().equals(updateMypageProduct.getP_ImageFileName())){
+//
+//        }
+//
+//
+//    }
+//
 
 
 
